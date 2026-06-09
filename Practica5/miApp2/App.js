cb2ff08cf7018ba0c48bc145fa37e255a3ac5bb8
Brando <brandomachucho22@gmail.com>
2026-06-09 01:10:33 -0600

    Repaso de Componentes

--- Practica5/miApp2/App.js
+++ Practica5/miApp2/App.js
@@ -3,11 +3,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {Saludo} from './components/Saludo';
 import {Saludo2} from './components/Saludo2';
+import {Perfil} from './components/Perfil';
 
 /* Zona 2: Main - Componentes */
 export default function App() {
   return (
-
 
     <View style={styles.container}>
       <Image source={require('./assets/Saludo.png')}/>
@@ -16,6 +16,8 @@ export default function App() {
       <Saludo/>
       <Text>------------------------------------</Text>
       <Saludo2/>
+      <Text>------------------------------------</Text>
+      <Perfil/>
       <StatusBar style="auto" />
 
     </View>
---
+++ Practica5/miApp2/components/Perfil.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+
+export const Perfil = () => {
+    return(
+
+        <View>
+            <Text>Brando Aldair Machucho Ambrocio</Text>
+            <Text>Ingeniería en Sistemas Computacionales</Text>
+            <Text>Programación Móvil</Text>
+            <Text>Noveno cuatrimestre</Text>
+        </View>
+
+    );
+}
